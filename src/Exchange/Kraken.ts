c39ef677a540ed53c03e0bd112065a6de16a01bb
alexandergw <alexgw@gmail.com>
2022-10-09 19:29:41 +0100

    Initial `Kraken` support

--- src/Exchange/Kraken.ts
+++ src/Exchange/Kraken.ts
@@ -1,7 +1,22 @@
-import { Exchange } from '../Exchange';
+import { Exchange, ExchangeData, ExchangeInterface } from '../Exchange';
 
-export class Kraken extends Exchange {
-	get () {
-		this.handle = '';
+export class Kraken extends Exchange implements ExchangeInterface {
+	constructor (
+		data: ExchangeData,
+	) {
+		super(data);
+
+		const KrakenClient = require('kraken-api');
+		this.api = new KrakenClient(data.key, data.secret);
 	}
+
+	async request (
+		data: object
+	) {
+		return '';
+	}
+
+	getPrimer () {}
+
+	getLatestTicker () {}
 }
