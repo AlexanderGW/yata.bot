0ede4e0b6f46840d7c0bfaf80cddbb7663d8385a
alexandergw <alexgw@gmail.com>
2022-10-09 23:10:39 +0100

    Assign a `Chart` instead of `Exchange`

--- src/Strategy.ts
+++ src/Strategy.ts
@@ -1,10 +1,12 @@
 import { Analysis } from "./Analysis";
-import { Exchange } from "./Exchange";
+import { Chart } from "./Chart";
 import { Pair } from "./Pair";
+
+const talib = require('./talib');
 
 export type StrategyData = {
 	analysis: Analysis[],
-	exchange: Exchange,
+	chart: Chart,
 	name?: string,
 	pair: Pair,
 
@@ -14,7 +16,7 @@ export type StrategyData = {
 
 export class Strategy implements StrategyData {
 	analysis: Analysis[];
-	exchange: Exchange;
+	chart: Chart;
 	name?: string;
 	pair: Pair;
 	timeframe: number;
@@ -23,10 +25,17 @@ export class Strategy implements StrategyData {
 		data: StrategyData,
 	) {
 		this.analysis = data.analysis;
-		this.exchange = data.exchange;
+		this.chart = data.chart;
 		if (data.name)
 			this.name = data.name;
 		this.pair = data.pair;
 		this.timeframe = data.timeframe > 0 ? data.timeframe : 0;
+	}
+
+	execute () {
+		
+		console.log(talib.explain({
+
+		}));
 	}
 }
