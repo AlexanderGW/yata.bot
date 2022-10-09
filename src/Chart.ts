d7d37aa13d6beb0c087a288407dbb7b68595bbe9
alexandergw <alexgw@gmail.com>
2022-10-09 23:29:11 +0100

    Merge `Chart` and `Candle`

--- src/Chart.ts
+++ src/Chart.ts
@@ -1,19 +1,49 @@
-import { Candle } from "./Candle";
 import { Exchange } from "./Exchange";
 
 export type ChartData = {
+	change: string[],
+	changePercent: string[],
+	close: string[],
+	closeTime: number[],
 	exchange: Exchange,
-	candle: Candle[],
+	high: string[],
+	low: string[],
+	open: string[],
+	openTime: number[],
+	tradeCount: number[],
+	volume: string[],
+	weightedAvePrice?: string[],
 };
 
 export class Chart implements ChartData {
-	candle: Candle[];
+	change: string[];
+	changePercent: string[];
+	close: string[];
+	closeTime: number[];
 	exchange: Exchange;
+	high: string[];
+	low: string[];
+	open: string[];
+	openTime: number[];
+	tradeCount: number[];
+	volume: string[];
+	weightedAvePrice?: string[];
 
 	constructor (
 		data: ChartData,
 	) {
-		this.candle = data.candle;
+		this.change = data.change;
+		this.changePercent = data.changePercent;
+		this.close = data.close;
+		this.closeTime = data.closeTime;
 		this.exchange = data.exchange;
+		this.high = data.high;
+		this.low = data.low;
+		this.open = data.open;
+		this.openTime = data.openTime;
+		this.tradeCount = data.tradeCount;
+		this.volume = data.volume;
+		if (data.weightedAvePrice)
+			this.weightedAvePrice = data.weightedAvePrice;
 	}
 }
