2ec6c7b6cc26bd8be89e572cc546daa942d57d42
alexandergw <alexgw@gmail.com>
2022-10-09 22:49:48 +0100

    Tie `Chart` to an `Exchange`

--- src/Chart.ts
+++ src/Chart.ts
@@ -1,15 +1,19 @@
 import { Candle } from "./Candle";
+import { Exchange } from "./Exchange";
 
 export type ChartData = {
+	exchange: Exchange,
 	candle: Candle[];
 };
 
 export class Chart implements ChartData {
 	candle: Candle[];
+	exchange: Exchange;
 
 	constructor (
 		data: ChartData,
 	) {
 		this.candle = data.candle;
+		this.exchange = data.exchange;
 	}
 }
