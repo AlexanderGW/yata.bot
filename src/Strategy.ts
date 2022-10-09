5788a6c8a95801f4005c8e017b111c0bef974919
alexandergw <alexgw@gmail.com>
2022-10-09 20:37:39 +0100

    Improvements to `StrategyData`

--- src/Strategy.ts
+++ src/Strategy.ts
@@ -1,19 +1,32 @@
 import { Analysis } from "./Analysis";
+import { Exchange } from "./Exchange";
 import { Pair } from "./Pair";
 
 export type StrategyData = {
-	pair: Pair,
 	analysis: Analysis[],
+	exchange: Exchange,
+	name?: string,
+	pair: Pair,
+
+	// Seconds
+	timeframe: number;
 }
 
 export class Strategy implements StrategyData {
-	pair: Pair;
 	analysis: Analysis[];
+	exchange: Exchange;
+	name?: string;
+	pair: Pair;
+	timeframe: number;
 
 	constructor (
 		data: StrategyData,
 	) {
-		this.pair = data.pair;
 		this.analysis = data.analysis;
+		this.exchange = data.exchange;
+		if (data.name)
+			this.name = data.name;
+		this.pair = data.pair;
+		this.timeframe = data.timeframe > 0 ? data.timeframe : 0;
 	}
 }
