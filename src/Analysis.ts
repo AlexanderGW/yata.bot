5b3c5b2e5ea86aeb68b2be6bd356688676697ba6
alexandergw <alexgw@gmail.com>
2022-10-09 23:22:46 +0100

    Support `config` on `Analysis`

--- src/Analysis.ts
+++ src/Analysis.ts
@@ -1,13 +1,17 @@
 export type AnalysisData = {
+	config?: object,
 	name: string,
 }
 
 export class Analysis implements AnalysisData {
+	config?: object;
 	name: string;
 
 	constructor (
 		data: AnalysisData,
 	) {
 		this.name = data.name;
+		if (data.config)
+			this.config = data.config;
 	}
 }
