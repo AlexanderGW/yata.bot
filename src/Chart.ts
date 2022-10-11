b50629977cb34346098cd5c71532808be75fcb1a
alexandergw <alexgw@gmail.com>
2022-10-11 21:49:04 +0100

    Expand `Chart` type constraints, and implement `refresh` method

--- src/Chart.ts
+++ src/Chart.ts
@@ -2,54 +2,94 @@ import { Exchange } from "./Exchange";
 import { Pair } from "./Pair";
 
 export type ChartData = {
-	change: string[],
-	changePercent: string[],
-	close: string[],
-	closeTime: number[],
 	exchange: Exchange,
-	high: string[],
-	low: string[],
-	open: string[],
-	openTime: number[],
 	pair: Pair,
 	timeframe: number, // Seconds
-	tradeCount: number[],
-	volume: string[],
+};
+
+export type ChartCandleData = {
+	change?: string[],
+	changePercent?: string[],
+	close?: string[],
+	closeTime?: number[],
+	high?: string[],
+	low?: string[],
+	open?: string[],
+	openTime?: number[],
+	tradeCount?: number[],
+	volume?: string[],
 	weightedAvePrice?: string[],
 };
 
-export class Chart implements ChartData {
-	change: string[];
-	changePercent: string[];
-	close: string[];
-	closeTime: number[];
+export class Chart implements ChartData, ChartCandleData {
+	change?: string[];
+	changePercent?: string[];
+	close?: string[];
+	closeTime?: number[];
 	exchange: Exchange;
-	high: string[];
-	low: string[];
-	open: string[];
-	openTime: number[];
+	high?: string[];
+	low?: string[];
+	open?: string[];
+	openTime?: number[];
 	pair: Pair;
 	timeframe: number;
-	tradeCount: number[];
-	volume: string[];
+	tradeCount?: number[];
+	volume?: string[];
 	weightedAvePrice?: string[];
 
 	constructor (
-		data: ChartData,
+		data: ChartData & ChartCandleData,
 	) {
-		this.change = data.change;
-		this.changePercent = data.changePercent;
-		this.close = data.close;
-		this.closeTime = data.closeTime;
+		if (data.change)
+			this.change = data.change;
+		if (data.changePercent)
+			this.changePercent = data.changePercent;
+		if (data.close)
+			this.close = data.close;
+		if (data.closeTime)
+			this.closeTime = data.closeTime;
 		this.exchange = data.exchange;
-		this.high = data.high;
-		this.low = data.low;
-		this.open = data.open;
-		this.openTime = data.openTime;
+		if (data.high)
+			this.high = data.high;
+		if (data.low)
+			this.low = data.low;
+		if (data.open)
+			this.open = data.open;
+		if (data.openTime)
+			this.openTime = data.openTime;
 		this.pair = data.pair;
 		this.timeframe = data.timeframe > 0 ? data.timeframe : 0;
-		this.tradeCount = data.tradeCount;
-		this.volume = data.volume;
+		if (data.tradeCount)
+			this.tradeCount = data.tradeCount;
+		if (data.volume)
+			this.volume = data.volume;
+		if (data.weightedAvePrice)
+			this.weightedAvePrice = data.weightedAvePrice;
+	}
+
+	refresh (
+		data: ChartCandleData,
+	) {
+		if (data.change)
+			this.change = data.change;
+		if (data.changePercent)
+			this.changePercent = data.changePercent;
+		if (data.close)
+			this.close = data.close;
+		if (data.closeTime)
+			this.closeTime = data.closeTime;
+		if (data.high)
+			this.high = data.high;
+		if (data.low)
+			this.low = data.low;
+		if (data.open)
+			this.open = data.open;
+		if (data.openTime)
+			this.openTime = data.openTime;
+		if (data.tradeCount)
+			this.tradeCount = data.tradeCount;
+		if (data.volume)
+			this.volume = data.volume;
 		if (data.weightedAvePrice)
 			this.weightedAvePrice = data.weightedAvePrice;
 	}
