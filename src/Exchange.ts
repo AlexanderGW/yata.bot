1f69f32be1d4006fb12c3c5ca360f765c722673d
alexandergw <alexgw@gmail.com>
2022-10-11 22:54:18 +0100

    Improved `Exchange` structure, with `Chart` support, and data store

--- src/Exchange.ts
+++ src/Exchange.ts
@@ -1,41 +1,131 @@
-import { Pair } from "./Pair";
+import { Chart, ChartCandleData } from "./Chart";
+import { uuid } from 'uuidv4';
+
+const fs = require('fs');
 
 export type ExchangeData = {
-	api?: object,
+	handle?: object,
 	name: string,
 	key?: string,
 	secret?: string,	
 }
 
 export interface ExchangeInterface {
-	request: (
-		data: object
+	primeChart: (
+		chart: Chart,
 	) => void;
 
-	getPrimer: (
-		pair: Pair,
-		interval: number,
-	) => void;
-
-	getLatestCandle: (
-		pair: Pair,
-		interval: number,
+	syncChart: (
+		chart: Chart,
 	) => void;
 }
 
-export class Exchange implements ExchangeData {
-	api?: object;
-	name: string;
+export interface ExchangeStorageInterface {
+	refreshChart: (
+		chart: Chart,
+		data: object,
+	) => void;
+}
 
+export class Exchange implements ExchangeData, ExchangeStorageInterface {
+	handle?: object;
+	name: string;
+	uuid: string;
+	
 	constructor (
 		data: ExchangeData,
 	) {
 		this.name = data.name;
+		this.uuid = uuid();
 	}
 
-	store (
-		data: object
+	compat (
+		chart: Chart,
 	) {
-		// TODO: Write to JSON files
+		if (chart.exchange.uuid === this.uuid)
+			return true;
+		return false;
+	}
+
+	refreshChart (
+		chart: Chart,
+		data: ChartCandleData
+	) {
+		chart.refresh(data);
+
+		return; // Skip logging
+
+		const pad = (value: number) =>
+			value.toString().length == 1
+			? `0${value}`
+			: value;
+
+		const now = new Date();
+
+		let pathParts = [
+			chart.exchange.name,
+			chart.pair.a.symbol + chart.pair.b.symbol,
+			now.getUTCFullYear(),
+			pad(now.getUTCMonth() + 1),
+			pad(now.getUTCDate()),
+		];
+		let path = pathParts.join('/');
+		// console.log(path);
+
+		// return;
+
+		let filenameParts = [
+			chart.exchange.name,
+			chart.pair.a.symbol + chart.pair.b.symbol,
+			now.getUTCFullYear(),
+			pad(now.getUTCMonth() + 1),
+			pad(now.getUTCDate()),
+			pad(now.getUTCHours()),
+			pad(now.getUTCMinutes()),
+			pad(now.getUTCSeconds()),
+		];
+		let filename = filenameParts.join('-');
+		// console.log(filename);
+
+		let responseJson = JSON.stringify(data);
+
+		let storagePath = `./storage/${path}`;
+		let storageFile = `${storagePath}/${filename}.json`;
+
+		try {
+			if (!fs.existsSync(storagePath)) {
+				fs.mkdirSync(
+					storagePath,
+					{
+						recursive: true
+					},
+					(err: object) => {
+						if (err)
+							throw err;
+
+						console.log(`Directory created: ${storagePath}`);
+					}
+				)
+			}
+		} catch (err) {
+			return console.error(err);
+		}
+
+        try {
+			fs.writeFile(
+				storageFile,
+				responseJson,
+				function (
+					err: object
+				) {
+					if (err)
+						throw err;
+					
+					console.log(`Stored: ${storageFile}`);
+				}
+			);
+		} catch (err) {
+			return console.error(err);
+		}
 	}
 }
