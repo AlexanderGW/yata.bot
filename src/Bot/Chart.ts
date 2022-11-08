9e4c350b8dbc1babd044b0ef5b33c0267f3f9ad5
alexandergw <alexgw@gmail.com>
2022-11-08 00:46:52 +0000

    Cleanup old `ChartCandleData`

--- src/Bot/Chart.ts
+++ src/Bot/Chart.ts
@@ -39,28 +39,12 @@ export class ChartItem implements ChartData {
 	constructor (
 		data: ChartData,
 	) {
-		// if (data.change)
-		// 	this.change = data.change;
-		// if (data.changePercent)
-		// 	this.changePercent = data.changePercent;
-		// if (data.close)
-		// 	this.close = data.close;
-		// if (data.closeTime)
-		// 	this.closeTime = data.closeTime;
 		this.dataset = data.dataset;
 		this.exchange = data.exchange;
-		// if (data.high)
-		// 	this.high = data.high;
 		if (data.lastUpdateTime)
 			this.lastUpdateTime = data.lastUpdateTime > 0 ? data.lastUpdateTime : 0;
 		else
 			this.lastUpdateTime = 0;
-		// if (data.low)
-		// 	this.low = data.low;
-		// if (data.open)
-		// 	this.open = data.open;
-		// if (data.openTime)
-		// 	this.openTime = data.openTime;
 		this.pair = data.pair;
 		if (data.pollTime)
 			this.pollTime = data.pollTime > 0 ? data.pollTime : 60;
@@ -70,46 +54,18 @@ export class ChartItem implements ChartData {
 			this.candleTime = data.candleTime > 0 ? data.candleTime : 3600;
 		else
 			this.candleTime = 3600;
-		// if (data.tradeCount)
-		// 	this.tradeCount = data.tradeCount;
 		this.uuid = data.uuid ?? uuid();
-		// if (data.volume)
-		// 	this.volume = data.volume;
-		// if (data.weightedAvePrice)
-		// 	this.weightedAvePrice = data.weightedAvePrice;
 	}
 
 	refresh (
 		data: ChartCandleData,
 	) {
-		// if (data.change)
-		// 	this.change = data.change;
-		// if (data.changePercent)
-		// 	this.changePercent = data.changePercent;
-		// if (data.close)
-		// 	this.close = data.close;
-		// if (data.closeTime)
-		// 	this.closeTime = data.closeTime;
-		// if (data.high)
-		// 	this.high = data.high;
 		this.dataset = data;
 		this.lastUpdateTime = data.openTime?.length
 			? (data.openTime[data.openTime.length - 1] * 1000)
 			: Date.now();
-		// let nnn = new Date(this.lastUpdateTime);
-		// Bot.log(`this.lastUpdateTime: ${nnn.toISOString()}`);
-		// if (data.low)
-		// 	this.low = data.low;
-		// if (data.open)
-		// 	this.open = data.open;
-		// if (data.openTime)
-		// 	this.openTime = data.openTime;
-		// if (data.tradeCount)
-		// 	this.tradeCount = data.tradeCount;
-		// if (data.volume)
-		// 	this.volume = data.volume;
-		// if (data.weightedAvePrice)
-		// 	this.weightedAvePrice = data.weightedAvePrice;
+		let now = new Date(this.lastUpdateTime);
+		Bot.log(`Chart '${this.uuid}' refresh`);
 	}
 }
 
