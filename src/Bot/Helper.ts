d00dc74721c75f43b9fcb0f3aef42414e8520bbe
alexandergw <alexgw@gmail.com>
2024-02-05 23:53:43 +0000

    Improve `isPercentage` and `countDecimals` input handling

--- src/Bot/Helper.ts
+++ src/Bot/Helper.ts
@@ -1,12 +1,18 @@
-export const isPercentage = (value: unknown): boolean => {
+export const isPercentage = (
+	value: unknown
+): boolean => {
 	const string = String(value);
 	return string.substring(string.length - 1) === '%'
 };
 
-export const countDecimals = (value: number) => {
-	if (Math.floor(value) === value)
+export const countDecimals = (
+	value: unknown
+) => {
+	const string = String(value);
+	const number = Number(value);
+	if (Math.floor(number) == number)
 		return 0;
-	const result = value.toString().split('.');
+	const result = string.split('.');
 	return result.length === 2 ? result[1].length : 0;
 };
 
