46c716e874dc3b88a6152465c53b5e73f051aa6a
alexandergw <alexgw@gmail.com>
2024-03-14 00:05:20 +0000

    Add sample LINK/BTC playbook

---
+++ playbook/sample-link-btc/sample-link-btc.ts
@@ -0,0 +1,51 @@
+import { Bot } from "../../src/Bot/Bot";
+import { OrderAction, OrderItem } from "../../src/Bot/Order";
+import { SubscriptionData } from "../../src/Bot/Subscription";
+
+export const actionLinkBtcBuy = async (
+	subscription: SubscriptionData
+) => {
+	Bot.log(`actionLinkBtcBuy`);
+	let linkBtcKrakenBuy: OrderItem = Bot.getItem('order:linkBtcKrakenBuy');
+	linkBtcKrakenBuy.update({
+		price: '-30%',
+		quantity: '0.1%',
+	});
+
+	const linkBtcKrakenBuyResult = await linkBtcKrakenBuy.execute(OrderAction.Open);
+	// const linkBtcKrakenBuyResult = await linkBtcKrakenBuy.execute(OrderAction.Edit);
+	// const linkBtcKrakenBuyResult = await linkBtcKrakenBuy.execute(OrderAction.Get);
+	// const linkBtcKrakenBuyResult = await linkBtcKrakenBuy.execute(OrderAction.Close);
+	
+	// TODO: To be refactored
+	Bot.__devPrepareNextStateOrder(
+		linkBtcKrakenBuy,
+		linkBtcKrakenBuyResult
+	);
+
+	Bot.exit();
+};
+
+export const actionLinkBtcSell = async (
+	subscription: SubscriptionData
+) => {
+	Bot.log(`actionLinkBtcSell`);
+	let linkBtcKrakenSell: OrderItem = Bot.getItem('order:linkBtcKrakenSell');
+	linkBtcKrakenSell.update({
+		price: '30%',
+		quantity: '0.1%',
+	});
+
+	const linkBtcKrakenSellResult = await linkBtcKrakenSell.execute(OrderAction.Open);
+	// const linkBtcKrakenSellResult = await linkBtcKrakenSell.execute(OrderAction.Edit);
+	// const linkBtcKrakenSellResult = await linkBtcKrakenSell.execute(OrderAction.Get);
+	// const linkBtcKrakenSellResult = await linkBtcKrakenSell.execute(OrderAction.Close);
+
+	// TODO: To be refactored
+	Bot.__devPrepareNextStateOrder(
+		linkBtcKrakenSell,
+		linkBtcKrakenSellResult
+	);
+
+	Bot.exit();
+};
